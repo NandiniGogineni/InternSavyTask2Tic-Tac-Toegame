e0031c1f99ef365ff235f7880be35a0a8b26748e
Nandini Gogineni <114126566+NandiniGogineni@users.noreply.github.com>
2023-08-14 22:05:05 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static char[][] board = new char[3][3];
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+
+        int movesLeft = 9;
+        while (movesLeft > 0) {
+            playMove();
+            printBoard();
+            if (checkWin()) {
+                System.out.println("Player " + currentPlayer + " wins!");
+                break;
+            }
+            switchPlayer();
+            movesLeft--;
+        }
+
+        if (movesLeft == 0) {
+            System.out.println("It's a draw!");
+        }
+    }
+
+    private static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    private static void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    private static void playMove() {
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
+
+        do {
+            System.out.println("Player " + currentPlayer + ", enter row (1-3) and column (1-3) separated by space: ");
+            row = scanner.nextInt() - 1;
+            col = scanner.nextInt() - 1;
+        } while (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != '-');
+
+        board[row][col] = currentPlayer;
+    }
+
+    private static boolean checkWin() {
+        return checkRows() || checkColumns() || checkDiagonals();
+    }
+
+    private static boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkDiagonals() {
+        return (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+               (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer);
+    }
+
+    private static void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+}
